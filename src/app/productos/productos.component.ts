ebd307b62a203a6b7957dff081733ad1423544af
Pablo De Cecco <pablomdececcorios@gmail.com>
2017-07-12 17:55:19 -0300

    pedidos

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -8,14 +8,45 @@ import { WebserviceService } from '../servicios/webservice.service';
   styleUrls: ['./productos.component.css']
 })
 export class ProductosComponent implements OnInit {
-  listadoProductos:any;
-  constructor(private WebserviceService: WebserviceService) { }
-
-  ngOnInit() {
-    this.WebserviceService.ListarProductos().then(data=>{
-      console.info(data)
-      this.listadoProductos=data;
-    })
+  listadoProductos: any;
+  pedidos: any;
+  lista: string[];
+  cantidad: number;
+  constructor(private WebserviceService: WebserviceService) {
+    this.lista = [];
+    this.cantidad = 0;
   }
 
+  ngOnInit() {
+    this.WebserviceService.ListarProductos().then(data => {
+      this.listadoProductos = data;
+    })
+  }
+  agregar(pizza) {
+
+    if (typeof this.pedidos != 'undefined') {
+      this.lista.push(pizza);
+    } else {
+      this.lista.push(pizza);
+    }
+    this.pedidos = this.lista;
+  }
+  eliminar(pizza) {
+    var BreakException = {};
+    try {
+    this.pedidos.forEach(element => {
+      console.info(element.idprod)
+      if (element.idprod == pizza.idprod) {
+        console.warn('false')
+        throw BreakException;
+
+      } else {
+        console.warn('true')
+
+      }
+    });
+     } catch (e) {
+      if (e !== BreakException) throw e;
+    }  
+  }
 }
